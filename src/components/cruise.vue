2e1f26bf24b3174f49c973b6c94bd9252916df96
parabellum <findqinya@gmail.com>
2022-04-07 18:05:57 +0800

    use  tween.js set box position

--- src/components/cruise.vue
+++ src/components/cruise.vue
@@ -7,12 +7,20 @@ import { onMounted, reactive, ref } from "vue";
 import * as THREE from "three";
 import threeUniversal from "../utils/threeUniversal.js";
 import Base3D from "../utils/base3D";
+import TWEEN from "@tweenjs/tween.js";
+
 const sceneContainer = ref(null);
 let data = reactive({
   base3D: {},
 });
 let pointsList = []; // 顶点数组
 let window_mouse = false; // 事件标识
+let first_Point = true;
+let points_group = new THREE.Group();
+points_group.name = "points";
+let lines_group = new THREE.Group();
+lines_group.name = "lines";
+
 onMounted(() => {
   initScene();
 });
@@ -20,6 +28,7 @@ onMounted(() => {
 function initScene() {
   data.base3D = new Base3D(sceneContainer.value);
   threeUniversal.addFloor(data.base3D.scene);
+  data.base3D.scene.add(points_group, lines_group);
   document.addEventListener("mousedown", onMouseDown, false);
   document.addEventListener("mousemove", onMouseMove, false);
   document.addEventListener("keydown", onKeyDown, false);
@@ -81,7 +90,7 @@ function mouseLeft(intersects) {
     });
     material.color.setHSL(1.0, 0.5, 0.7); // 设置点颜色
     const endpoint = new THREE.Points(geometry, material);
-    data.base3D.scene.add(endpoint);
+    points_group.add(endpoint);
     /* 创建线段 */
     let lineGeometry = new THREE.BufferGeometry().setFromPoints(pointsList);
     let lineMaterial = new THREE.LineBasicMaterial({
@@ -93,31 +102,27 @@ function mouseLeft(intersects) {
       let line = new THREE.Line(lineGeometry, lineMaterial);
       line.castShadow = true;
       pointsList.shift();
-      data.base3D.scene.add(line);
+      lines_group.add(line);
     }
-
-    /*   /!* 一条连续的线段的开始端的球体几何 *!/
-    if (first_sphere) {
-      /!* 创建球体 *!/
-      let sphereGeometry = new THREE.SphereGeometry(5, 20, 20);
+    /* 一条连续的线段的开始端的球体几何 */
+    if (first_Point) {
+      /* 创建球体 */ let boxGeometry = new THREE.BoxGeometry(5, 2, 15);
       let sphereMaterial = new THREE.MeshLambertMaterial({
         color: 0xb23aee,
       });
-
-      let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-      sphere.position.set(intersects.x, intersects.y + 5, intersects.z);
-      scene.add(sphere);
-
-      first_sphere = false;
+      let sphere = new THREE.Mesh(boxGeometry, sphereMaterial);
+      sphere.position.set(x, y, z);
+      sphere.name = "box";
+      data.base3D.scene.add(sphere);
+      first_Point = false;
     }
-
-    scene.add(points);*/
   }
 }
 
 function mouseRight() {
   removeListener();
   deleteLast(); // 恢复监听事件
+  first_Point = true;
 }
 
 function onMouseMove(event) {
@@ -139,6 +144,12 @@ function onMouseMove(event) {
   }
 }
 
+/**
+ * 键盘事件
+ * ESC
+ * space
+ * @param event
+ */
 function onKeyDown(event) {
   if (event.keyCode === 27) {
     //ESC键
@@ -154,6 +165,89 @@ function onKeyDown(event) {
   }
   if (event.keyCode === 32) {
     // space键
+    /* 开始动画的时候不允许 esc 操作， 所以此时移除键盘事件 */
+    document.removeEventListener("keydown", onKeyDown, false);
+    /* 当 window_mouse 为 false 时（为false时说明结束绘制）开始移动，否则不移动*/
+    if (!window_mouse) {
+      console.log("start move！");
+      let points = [];
+      let box = null;
+      data.base3D.scene.traverse((object) => {
+        if (object.type === "Mesh" && object.name === "box") box = object;
+      });
+      /* 取出点 */
+      data.base3D.scene.traverse((object) => {
+        if (object.type === "Group" && object.name === "points")
+          points.push(object.children);
+      });
+
+      /* 动画数组 */
+      let tweens = [];
+
+      /* 当点的数量大于 1 的时候才可以进行动画 */
+      if (points.length > 1) {
+        for (let i = 0; i < points.length; i++) {
+          tweens.push(
+            new TWEEN.Tween(box.position)
+          ); /* 此时创建了 points.length 个 TWEEN 对象 */
+
+          if (i < points.length - 1) {
+            tweens[i].to(
+              {
+                x: points[i + 1].boundingSphere.center.x,
+                z: points[i + 1].boundingSphere.center.z,
+              },
+              2000
+            ); /* 规定去向 */
+          }
+        }
+        /* 是动画链接到一起 */
+        for (let i = 0; i < points.length - 1; i++) {
+          tweens[i].chain(tweens[i + 1]);
+        }
+
+        /* 从第一个开始执行 */
+        tweens[0].start();
+
+        /* onUpdate 类似 animation 执行动画的过程中所做的事 在tween每次被更新后执行 */
+        tweens[0].onUpdate((object) => {
+          console.log("elapsed-time: " + object);
+          console.log(
+            "x: " +
+              Math.round(box.position.x) +
+              " ,y: " +
+              Math.round(box.position.y) +
+              " ,z:" +
+              Math.round(box.position.z)
+          );
+        });
+
+        /*    /!* 走完路程执行的操作 *!/
+        tweens[tweens.length - 2].onComplete(function () {
+          console.log("球体寻线结束！");
+
+          for (let i = 0; i < scene.children.length; i++) {
+            let object = scene.children[i];
+
+            if (
+              object.type === "Points" ||
+              object.castShadow === true ||
+              object.geometry instanceof THREE.SphereGeometry
+            ) {
+              scene.remove(object);
+              /!* i-- 作用：用来控制scene.children.length不断减小而 i 却不断变大的问题，否则会导致无法完全删除元素 *!/
+              i--;
+            }
+          }
+
+          /!* 移除完线段、点、球结束后 再次添加点击事件 *!/
+          window.addEventListener("mousedown", onMouseDown, false);
+
+          /!* 动画结束之后， 在添加键盘事件 *!/
+          window.addEventListener("keydown", onKeyDown, false);
+        });*/
+      }
+    }
   }
 }
 
@@ -176,7 +270,9 @@ function removeListener() {
   }
 }
 
-/* 当场景中只有一个球和一个点的时候执行删除操作 */
+/**
+ * 当场景中只有一个球和一个点的时候执行删除操作
+ */
 function deleteLast() {
   let i = 0;
   data.base3D.scene.traverse((object) => {
